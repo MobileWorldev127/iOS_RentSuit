6aa768a5dbba328aba1d61155fad7cafe1ca9fe5
MobileWorldev127 <alexander.soft@hotmail.com>
2020-06-13 17:52:33 -0500

    add rented detail screen

---
+++ RentaSuit/Models/RentedProductDetail.swift
@@ -0,0 +1,9 @@
+//
+//  RentedProductDetail.swift
+//  RentaSuit
+//
+//  Created by macos on 6/13/20.
+//  Copyright © 2020 MacBook Pro. All rights reserved.
+//
+
+import Foundation
---
+++ RentaSuit/ViewsControllers/Rented/RentedProductDetailsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RentedProductDetailsViewController.swift
+//  RentaSuit
+//
+//  Created by macos on 6/12/20.
+//  Copyright © 2020 MacBook Pro. All rights reserved.
+//
+
+import Foundation
